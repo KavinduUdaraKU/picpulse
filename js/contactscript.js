2467e32ed84a6df204b625f2131c6cd3ad82bf9b
KavinduUdaraKU <165522264+KavinduUdaraKU@users.noreply.github.com>
2025-01-23 10:05:18 +0530

    Create contactscript.js

---
+++ js/contactscript.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_bv089ae';
+   const templateID = 'template_oynusna';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
